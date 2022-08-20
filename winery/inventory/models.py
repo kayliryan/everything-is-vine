2756d18d9748ce81dfbe6093f9269c904e1206b1
Richard Scott <rich5d@icloud.com>
2022-08-20 11:30:06 -0700

    added user model

--- winery/inventory/models.py
+++ winery/inventory/models.py
@@ -27,3 +27,19 @@ class Wine(models.Model):
     price = models.FloatField()
     picture_url = models.URLField(max_length=220, null=True)
     quantity = models.SmallIntegerField()
+    
+    
+class User(models.Model):
+    id = models.SmallIntegerField()
+    name = models.CharField(max_length=110)
+    address = models.CharField(max_length=254)
+    phone = models.CharField(max_length=9)
+    email =models.CharField(max_length=110)
+    winery_id = models.ForeignKey(
+        Winery,
+        related_name="+",
+        on_delete=models.PROTECT
+    )
+    membership_level = models.SmallIntegerField()
+    employee = models.BooleanField()
+    
