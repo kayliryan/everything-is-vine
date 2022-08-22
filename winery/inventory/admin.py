da6bfe9ced01b1a02972978997597106f26cc504
Richard Scott <rich5d@icloud.com>
2022-08-22 08:56:06 -0700

    registered models in admin

--- winery/inventory/admin.py
+++ winery/inventory/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Wine, Winery, User
 # Register your models here.
+class WineAdmin(admin.ModelAdmin):
+    pass
+
+class WineryAdmin(admin.ModelAdmin):
+    pass
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Wine, WineAdmin)
+admin.site.register(Winery, WineryAdmin)
+admin.site.register(User, UserAdmin)
