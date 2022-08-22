b25b3cc8d447ac6362d81a271809aa24e7b43033
Richard Scott <rich5d@icloud.com>
2022-08-22 10:47:03 -0700

    moved user admin

--- winery/inventory/admin.py
+++ winery/inventory/admin.py
@@ -7,9 +7,6 @@ class WineAdmin(admin.ModelAdmin):
 class WineryAdmin(admin.ModelAdmin):
     pass
 
-class UserAdmin(admin.ModelAdmin):
-    pass
 
 admin.site.register(Wine, WineAdmin)
 admin.site.register(Winery, WineryAdmin)
-admin.site.register(User, UserAdmin)
