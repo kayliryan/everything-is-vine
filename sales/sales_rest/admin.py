02b8c8669b47bf2cf5d71a7dbee1927e78ffcdd6
Kayli <kayliryan@gmail.com>
2022-08-22 16:35:04 -0700

    Models updated. Migrations made. Containers working

--- sales/sales_rest/admin.py
+++ sales/sales_rest/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Order, ShoppingItem
-
+from .models import Order, ShoppingItem, WineryVO, WineVO
 # Register your models here.
 class OrderAdmin(admin.ModelAdmin):
     pass
@@ -8,6 +7,14 @@ class OrderAdmin(admin.ModelAdmin):
 class ShoppingItemAdmin(admin.ModelAdmin):
     pass
 
+class WineryVOAdmin(admin.ModelAdmin):
+    pass
+
+class WineVOAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Order, OrderAdmin)
 admin.site.register(ShoppingItem, ShoppingItemAdmin)
+admin.site.register(WineryVO, WineryVOAdmin)
+admin.site.register(WineVO, WineVOAdmin)
 
--- winery/inventory/admin.py
+++ winery/inventory/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Wine, Winery, User
+from .models import Wine, Winery
 # Register your models here.
 class WineAdmin(admin.ModelAdmin):
     pass
