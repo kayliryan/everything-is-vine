09d7710035fad9ca7a08d584eb85a22d8a0a7f97
Richard Scott <rich5d@icloud.com>
2022-08-22 16:41:22 -0700

    removed models to merge with kayli's working branch

--- sales/sales_rest/admin.py
+++ sales/sales_rest/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
-from .models import Order, ShoppingItem
 
-# Register your models here.
-class OrderAdmin(admin.ModelAdmin):
-    pass
-
-class ShoppingItemAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Order, OrderAdmin)
-admin.site.register(ShoppingItem, ShoppingItemAdmin)
 
--- winery/inventory/admin.py
+++ winery/inventory/admin.py
@@ -1,12 +1,2 @@
 from django.contrib import admin
-from .models import Wine, Winery, User
-# Register your models here.
-class WineAdmin(admin.ModelAdmin):
-    pass
 
-class WineryAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Wine, WineAdmin)
-admin.site.register(Winery, WineryAdmin)
