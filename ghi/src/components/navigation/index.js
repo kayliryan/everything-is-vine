df4fe56b9bf7e1cd32b7675748fba785a384f48b
Shadan <shadanamini@gmail.com>
2022-08-23 12:14:23 -0700

    added components folder and began work on navigation bar

---
+++ ghi/src/components/navigation/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NavigationBar = () => {
+    return (
+        <Nav>
+            <NavigationBarContainer>
+            </NavigationBarContainer>
+        </Nav>
+    );
+};
