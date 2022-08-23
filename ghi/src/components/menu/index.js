77252e13777a68103e61aa4d9b5ace8796e5f7cc
Shadan <shadanamini@gmail.com>
2022-08-23 16:01:31 -0700

    worked on index for menu

--- ghi/src/components/menu/index.js
+++ ghi/src/components/menu/index.js
@@ -11,6 +11,27 @@ const Menu = () => {
         <Icon>
             <CloseIcon />
         </Icon>
+        <MenuWrapper>
+            <MenuSubMenu>
+                <MenuLink to='wines'>
+                    Wines
+                </MenuLink>
+                <MenuLink to='contact'>
+                    Contact
+                </MenuLink>
+                <MenuLink to='shopping'>
+                    Cart
+                </MenuLink>
+                <MenuLink to='signup'>
+                    Sign Up
+                </MenuLink>
+            </MenuSubMenu>
+            <MenuButtonWrap>
+                <MenuRoute to='/signin'>
+                    Sign In
+                </MenuRoute>
+            </MenuButtonWrap>
+        </MenuWrapper>
     </MenuContainer>
   );
 };
