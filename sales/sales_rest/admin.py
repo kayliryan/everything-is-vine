a62375ff9eedfa82465fb66fb37110539740fe00
Jennifer <xjenniferwux@gmail.com>
2022-08-24 15:59:18 -0700

    Resolve merge conflict

--- sales/sales_rest/admin.py
+++ sales/sales_rest/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, ShoppingItem, WineryVO, WineVO
+from .models import Order, ShoppingItem, WineVO
 # Register your models here.
 class OrderAdmin(admin.ModelAdmin):
     pass
@@ -7,14 +7,14 @@ class OrderAdmin(admin.ModelAdmin):
 class ShoppingItemAdmin(admin.ModelAdmin):
     pass
 
-class WineryVOAdmin(admin.ModelAdmin):
-    pass
+# class WineryVOAdmin(admin.ModelAdmin):
+#     pass
 
 class WineVOAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Order, OrderAdmin)
 admin.site.register(ShoppingItem, ShoppingItemAdmin)
-admin.site.register(WineryVO, WineryVOAdmin)
+# admin.site.register(WineryVO, WineryVOAdmin)
 admin.site.register(WineVO, WineVOAdmin)
 
