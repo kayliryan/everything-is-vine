7b02eeae3ae8ba807ccb2e1fb49484d032ad2cf7
Jennifer <xjenniferwux@gmail.com>
2022-08-25 10:16:04 -0700

    Comment out view for list of wines from specific winery

--- sales/sales_rest/api_urls.py
+++ sales/sales_rest/api_urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
 from .api_views import (
+    # api_list_wines,
     api_show_wine
 )
 
 
 urlpatterns = [
-    # path("wines/<int:pk>/", 
-    # api_show_wine , name="api_show_wine"),
+
+    # list of wines from a specific winery
+    # path("wineries/<int:pk1>/wines/", api_list_wines, name="api_list_wines"),
+
+    # detail of specific wine from a specific winery
     path("wineries/<int:pk1>/wines/<int:pk2>/", 
     api_show_wine , name="api_show_wine"),
 ]
