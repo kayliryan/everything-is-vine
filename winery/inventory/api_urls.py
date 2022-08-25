a16f1c466cb9c0e1f2a31acdf36e449e1548af64
Shadan <shadanamini@gmail.com>
2022-08-25 16:42:04 -0700

    fixed another conflict

--- winery/inventory/api_urls.py
+++ winery/inventory/api_urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-<<<<<<< HEAD
 from .api_views import (api_list_wines, api_list_winery, api_winery, api_list_all_wines)
 
 urlpatterns = [
@@ -11,15 +10,4 @@ urlpatterns = [
     # path(“appts/<int:pk>/“, api_appointment_details, name=“api_show_appt”),
     # path(“byvin/<str:vin>/“, api_list_appointments_by_vin, name=“api_list_apptbyvin”),
     # path(“techs/<int:pk>/“, api_technician_details, name=“api_show_tech”),
-=======
-
-from .api_views import (api_list_wines)
-
-urlpatterns = [
-    path("wines/", api_list_wines, name="api_list_wines"),
-    # path("techs/", api_list_technicians, name="api_list_technicians"),
-    # path("appts/<int:pk>/", api_appointment_details, name="api_show_appt"),
-    # path("byvin/<str:vin>/", api_list_appointments_by_vin, name="api_list_apptbyvin"),
-    # path("techs/<int:pk>/", api_technician_details, name="api_show_tech"),
->>>>>>> main
 ]
