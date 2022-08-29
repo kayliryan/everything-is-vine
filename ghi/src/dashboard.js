16157496c5f255857c02856220a0fd9d8ea6cbc5
Shadan <shadanamini@gmail.com>
2022-08-29 14:13:10 -0700

    styling changes to winery dashboard

--- ghi/src/dashboard.js
+++ ghi/src/dashboard.js
@@ -82,7 +82,7 @@ async componentDidMount() {
 render() {
     return (
     <>
-        <div className="px-4 py-5 my-5 mt-0 text-center bg-info">
+        <div className="px-4 py-5 my-5 mt-0 text-center bg-success">
         <img className="bg-white rounded shadow d-block mx-auto mb-4" src="/logo.svg" alt="" width="600" />
         <h1 className="display-5 fw-bold">Our Winery Collective</h1>
         <div className="col-lg-6 mx-auto">
