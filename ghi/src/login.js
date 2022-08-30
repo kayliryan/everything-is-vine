2f3417eee08567520c62f6af4cb66bc497fb9215
Boden <boden.bradley@gmail.com>
2022-08-30 16:42:50 -0700

    improved css for login/signup forms and added wine favicon

--- ghi/src/login.js
+++ ghi/src/login.js
@@ -2,6 +2,7 @@ import './App.css';
 import React,{useState} from 'react';
 import { useToken } from './auth';
 import { useParams } from 'react-router-dom';
+import './login.css';
 
 
 function Login() {
@@ -35,32 +36,50 @@ const submitLogoutHandler = e => {
     }
 
 return (
-    <div className="container">
-        <form onSubmit={submitHandler} className="form-signin">
-        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-        <div className="row">
-            <div className="col">
-            <input
-                type="text"
-                value={username}
-                name="username"
-                onChange={changeHandler}
-                placeholder="Enter Username"
-            />
-            <input
-                type="password"
-                value={password}
-                name="password"
-                onChange={changeHandler}
-                placeholder="Enter Password"
-            />
-            <input type="submit" value="Login" />
+    // <div className="container">
+    //     <form onSubmit={submitHandler} className="form-signin">
+    //     <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+    //     <div className="row">
+    //         <div className="col">
+    //         <input
+    //             type="text"
+    //             value={username}
+    //             name="username"
+    //             onChange={changeHandler}
+    //             placeholder="Enter Username"
+    //         />
+    //         <input
+    //             type="password"
+    //             value={password}
+    //             name="password"
+    //             onChange={changeHandler}
+    //             placeholder="Enter Password"
+    //         />
+    //         <input type="submit" value="Login" />
+    //         </div>
+    //     </div>
+    //     </form>
+    //     <button type="submit" onClick={submitLogoutHandler}>Logout</button>
+    //</div>
+        <div className="wrapper fadeInDown">
+            <div id="formContent">
+                <div className="fadeIn first">
+                </div>
+                <form onSubmit={submitHandler}>
+                    <input type="text" id="username" className="fadeIn second" name="username" placeholder="Enter Username" 
+                    value={username} onChange={changeHandler}/>
+                    <input type="password" id="password" className="fadeIn third" name="password" placeholder="Enter Password" 
+                    value={password} onChange={changeHandler}/>
+                    <input type="submit" className="fadeIn fourth" value="Sign In" />
+                </form>
             </div>
-        </div>
-        </form>
-        <button type="submit" onClick={submitLogoutHandler}>Logout</button>
-    </div>
+            <button type="submit" onClick={submitLogoutHandler}>Logout</button>
+            </div>
+    
 );
 }
 
 export default Login;
+
+
+
