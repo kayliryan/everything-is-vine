3fc5894e9144f90f3a237575396d5132edfeaf93
Kayli <kayliryan@gmail.com>
2022-08-30 12:25:32 -0700

    No major changes since yesterday except adding in more reducer functions that will need to be modified. Pushing to main so others can pull

--- ghi/src/WineDetails.js
+++ ghi/src/WineDetails.js
@@ -8,7 +8,7 @@ import {useGetWineDetailsQuery} from './store/salesApi';
 function GetWine() {
   let { winery_id, winevo_id } = useParams();
   const {data, error, isLoading} = useGetWineDetailsQuery({winery_id, winevo_id});
-  const [shoppingItems, updateShoppingItems] = useState([data]);
+  const [shoppingItems, updateShoppingItems] = useState([]);
 
   if (isLoading) {
     return (
@@ -16,36 +16,43 @@ function GetWine() {
     );
   }
 
+
+async function addToShoppingItems(){
+  updateShoppingItems(data)
+  }
+
+  
+
 // const WineDetails = () => {
   return (
-    <>
-    <p>{data.brand}</p>
-    </>
+    // <>
+    // <p>{data.brand}</p>
+    // </>
     
     // <p>{data.winevo.brand}</p>
-    // <div className="container-fluid">
-    //     <div className="card mx-auto col-md-3 col-10 mt-5">
-    //         <img
-    //         className="mx-auto img-thumbnail"
-    //         src="https://i.imgur.com/pjITBzX.jpg"
-    //         width="auto"
-    //         height="auto"
-    //         />
-    //         <div className="card-body text-center mx-auto">
-    //             <div className="cvp">
-    //                 <h5 className="card-title font-weight-bold">Brand, Year</h5>
-    //                 <h6>Varietal, Region,</h6>
-    //                 <h6>Volume, ABV</h6>
-    //                 <p className="card-text">Price</p>
-    //                 <p>Description</p>
-    //                 <button href="#" class="btn btn-info btn-lg">
-    //                   <span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart
-    //                 </button>
+    <div className="container-fluid">
+        <div className="card mx-auto col-md-3 col-10 mt-5">
+            <img
+            className="mx-auto img-thumbnail"
+            src="https://i.imgur.com/pjITBzX.jpg"
+            width="auto"
+            height="auto"
+            />
+            <div className="card-body text-center mx-auto">
+                <div className="cvp">
+                    <h5 className="card-title font-weight-bold">Brand, Year</h5>
+                    <h6>Varietal, Region,</h6>
+                    <h6>Volume, ABV</h6>
+                    <p className="card-text">Price</p>
+                    <p>Description</p>
+                    <button onClick = {addToShoppingItems} href="#" className="btn btn-info btn-lg">
+                      <span className="glyphicon glyphicon-shopping-cart"></span> Shopping Cart
+                    </button>
 
-    //             </div>
-    //         </div>
-    //     </div>
-    // </div>
+                </div>
+            </div>
+        </div>
+    </div>
   );
 };
 export default GetWine;
