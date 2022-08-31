1afa8edaa8c3fb0aaa6d9779cf5fa96a0694e2ec
Jennifer <xjenniferwux@gmail.com>
2022-08-30 22:14:54 -0700

    Update journal entry for 8/30/22

--- ghi/src/store/store.js
+++ ghi/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/dist/query'
 import { salesApi } from './salesApi'
+// import { cartApi } from './cartApi'
 
 
 export const store = configureStore({
     reducer: {
         [salesApi.reducerPath]: salesApi.reducer,
+        // cartApi.reducerPath
     },
     middleware: getDefaultMiddleware => 
         getDefaultMiddleware().concat(salesApi.middleware),
