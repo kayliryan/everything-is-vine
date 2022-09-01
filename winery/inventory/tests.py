526c6df1cc6b19f8bb674580d9bb5ab9af23a135
Shadan <shadanamini@gmail.com>
2022-09-01 15:42:28 -0700

    second unit test working

--- winery/inventory/tests.py
+++ winery/inventory/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from .models import Wine
+from .models import Wine, Winery
 
 
-class TestViews(TestCase):
+class TestWinesViews(TestCase):
     def setUp(self):
         Wine.objects.create(
             brand="test_brand",
@@ -15,7 +15,20 @@ class TestViews(TestCase):
             quantity=3,
         )
 
-    def test_list_accounts_GET(self):
+    def test_list_wines(self):
         client = Client()
         response = client.get(reverse("api_list_all_wines"))
+        self.assertEquals(response.status_code, 200)
+
+class TestWineriesViews(TestCase):
+    def setUp(self):
+        Winery.objects.create(
+            name="test_name",
+            address="test_address",
+            description="test_description",
+        )
+
+    def test_list_wineries(self):
+        client = Client()
+        response = client.get(reverse("api_list_wineries"))
         self.assertEquals(response.status_code, 200)
