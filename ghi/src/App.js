4a55727b6f4ec18ca411c60b2ab59824c732f70c
Jennifer <xjenniferwux@gmail.com>
2022-09-01 06:05:08 -0700

    Added ShoppingCartTest page to test code involving state

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -7,6 +7,7 @@ import Login from './login';
 import SignUp from './signup';
 import WineryList from './dashboard';
 import { AuthProvider } from './auth';
+// import ShoppingCartTest from './ShoppingCartTest'
 
 function App(props) {
 
@@ -23,6 +24,10 @@ function App(props) {
             {/* <Route path="wineries/:id/" element={<WineList />} /> */}
             <Route path="wineries/:id/login/" element={<Login />} />
             <Route path="wineries/:id/signup/" element={<SignUp />} />
+
+
+            {/* <Route path="wineries/:id/shoppingcarttest/" element={<ShoppingCartTest />} /> */}
+
 
           </Routes>
         </div>
