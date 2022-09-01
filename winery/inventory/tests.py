c84231851ab247fc8ee3f68c53971ce6618c0aa4
Shadan <shadanamini@gmail.com>
2022-09-01 15:22:02 -0700

    got one unit test working

--- winery/inventory/tests.py
+++ winery/inventory/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Wine
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    def setUp(self):
+        Wine.objects.create(
+            brand="test_brand",
+            year=2007,
+            varietal="test_varietal",
+            abv=8.5,
+            volume=123,
+            price=40,
+            quantity=3,
+        )
+
+    def test_list_accounts_GET(self):
+        client = Client()
+        response = client.get(reverse("api_list_all_wines"))
+        self.assertEquals(response.status_code, 200)
