ac37be1409b9061c32d196f75004fa50d1358a3d
Jennifer <xjenniferwux@gmail.com>
2022-09-01 17:10:57 -0700

    Save merge

--- ghi/src/WineDetails.js
+++ ghi/src/WineDetails.js
@@ -43,7 +43,6 @@ function GetWine() {
 
 
   return (
-<<<<<<< HEAD
     // <>
     // <p>{data.brand}</p>
     // </>
@@ -76,41 +75,3 @@ function GetWine() {
   );
 };
 export default GetWine;
-=======
-    <>
-      <Card />
-    </>
-  );
-};
-export default GetWine;
-
-
-{/* <div className="container-fluid mx-auto my-auto mt-5">
-<div className="card mx-auto col-lg col-4 mt-8">
-    <img
-    className="mx-auto img-thumbnail"
-    src="https://i.imgur.com/pjITBzX.jpg"
-    width="auto"
-    height="auto"
-    />
-    <div className="card-body text-center mx-auto">
-        <div className="cvp">
-          <div className="col-sm">
-            <h3 className="card-title font-weight-bold">{data.brand}</h3>
-          </div>
-            <h3>{data.year}</h3>
-            <h3>{data.varietal}</h3>
-            <h3>Region: {data.region}</h3>
-            <h3>Alcohol: {data.abv}%</h3>
-            <h3>Volume: {data.volume} mL</h3>
-            <p className="card-text">Price</p>
-            <p>{data.description}</p>
-            <p>Quantity</p>
-            <button onClick = {addToShoppingItems} href="#" className="btn btn-success btn-lg">
-              <span className="align-items-center">Shopping Cart <AiOutlineShoppingCart/></span> 
-            </button>
-        </div>
-    </div>
-</div>
-</div> */}
->>>>>>> main
