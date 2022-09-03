cc3960613a902c256860895592f35eef89d441ac
Richard Scott <rich5d@icloud.com>
2022-09-02 19:27:27 -0700

    9-2022

--- ghi/src/wines.js
+++ ghi/src/wines.js
@@ -2,6 +2,47 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function WineColumn(props) {
+    useEffect(() => {
+        const [wineColumns,setWineColumns] = useState(
+            [[], [], []]
+        )
+        const {id} = useParams()
+    
+        async function fetchWines(){
+            const url = `http://localhost:8000/api/wineries/${id}/wines/`;
+    
+            try {
+            const response = await fetch(url);
+            if (response.ok) {
+                const data = await response.json();
+    
+                const wineColumns = [[], [], []];
+                let list = []
+                for (let wine of data.wines){
+                    list.push(wine)
+                }
+                console.log(list)
+    
+                let i = 0;
+                for (const wine of list) {
+                if (wine) {
+                    wineColumns[i].push(wine);
+                    i = i + 1;
+                    if (i > 2) {
+                    i = 0;
+                    }
+                } else {
+                    console.error(wine);
+                }
+                }
+    
+                console.log(wineColumns)
+                setWineColumns(wineColumns);
+            }
+            } catch (e) {
+            console.error(e);
+            }
+        }}, [])
 return (
     <div className="col">
     {props.list.map(data => {
@@ -29,76 +70,35 @@ return (
 );
 }
 
-useEffect(() => {
-    const [wineColumns,setWineColumns] = useState(
-        [[], [], []]
-    )
-    const {id} = useParams()
-
-    async function fetchWines(){
-        const url = `http://localhost:8000/api/wineries/${id}/wines/`;
-
-        try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-
-            const wineColumns = [[], [], []];
-            let list = []
-            for (let wine of data.wines){
-                list.push(wine)
-            }
-            console.log(list)
-
-            let i = 0;
-            for (const wine of list) {
-            if (wine) {
-                wineColumns[i].push(wine);
-                i = i + 1;
-                if (i > 2) {
-                i = 0;
-                }
-            } else {
-                console.error(wine);
-            }
-            }
-
-            console.log(wineColumns)
-            setWineColumns(wineColumns);
-        }
-        } catch (e) {
-        console.error(e);
-        }
-    }}, [])
 
     // useEffect( () => {fetchWines()},[]) #unnecessary replaced it on line 32
 
 
 
 
-    return (
-        <>
-            <div className="px-4 py-5 my-5 mt-0 text-center bg-success">
-            <img className="bg-white rounded shadow d-block mx-auto mb-4" src="/logo.svg" alt="" width="600" />
-            <h1 className="display-5 fw-bold">Our Wines</h1>
-            <div className="col-lg-6 mx-auto">
-                <p className="lead mb-4">
-                Please enjoy a selection of our finest wines. 
-                </p>
-            </div>
-            </div>
-            <div className="container">
-            <div className="row">
-                {wineColumns.map((wineList, index) => {
-                return (
-                    <WineColumn id={id} key={index} list={wineList} />
-                );
-                })}
-            </div>
-            </div>
-        </>
-    );
+    // return (
+    //     <>
+    //         <div className="px-4 py-5 my-5 mt-0 text-center bg-success">
+    //         <img className="bg-white rounded shadow d-block mx-auto mb-4" src="/logo.svg" alt="" width="600" />
+    //         <h1 className="display-5 fw-bold">Our Wines</h1>
+    //         <div className="col-lg-6 mx-auto">
+    //             <p className="lead mb-4">
+    //             Please enjoy a selection of our finest wines. 
+    //             </p>
+    //         </div>
+    //         </div>
+    //         <div className="container">
+    //         <div className="row">
+    //             {wineColumns.map((wineList, index) => {
+    //             return (
+    //                 <WineColumn id={id} key={index} list={wineList} />
+    //             );
+    //             })}
+    //         </div>
+    //         </div>
+    //     </>
+    // );
 
-}
+
 
 export default WineList;
