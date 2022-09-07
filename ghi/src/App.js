15f2494519321afaa582955a9221ec1d73fd269b
Jennifer <xjenniferwux@gmail.com>
2022-09-07 16:19:28 -0700

    Fix Routes from merge

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -9,10 +9,7 @@ import WineryList from './dashboard';
 import { AuthProvider } from './auth';
 import NavbarLayout from './navLayout';
 import Contact from './contact';
-
-
 import ShoppingCartTest from './ShoppingCart';
-
 
 function App(props) {
 
@@ -20,19 +17,6 @@ function App(props) {
 
     <AuthProvider>
       <BrowserRouter>
-        {/* <Nav /> */}
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<WineryList />} />
-            <Route path="wineries/:id/wines/" element={<WineList />} />
-            <Route path="/wineries/:winery_id/wines/:winevo_id/" element={<GetWine />} />
-            {/* <Route path="wineries/:id/" element={<WineList />} /> */}
-            <Route path="wineries/:id/login/" element={<Login />} />
-            <Route path="wineries/:id/signup/" element={<SignUp />} />
-            <Route path="wineries/:id/shoppingcarttest/" element={<ShoppingCartTest />} />
-
-          </Routes>
-        </div>
           <div className="container">
             <Routes>
               <Route path="/" element={<WineryList />} />
@@ -45,6 +29,7 @@ function App(props) {
                 <Route path="wineries/:id/" element={<Winery />} /> 
                 <Route path="/wineries/:id/login/" element={<Login />} />
                 <Route path="/wineries/:id/signup/" element={<SignUp />} />
+                <Route path="/wineries/:id/shoppingcarttest/" element={<ShoppingCartTest />} />
               </Route>
             </Routes>
           </div>
