77077a3fade28fdacbf3f9fbb06cbef3640ef515
Jennifer <xjenniferwux@gmail.com>
2022-09-14 15:24:33 -0700

    Models updated after merge conflicts fixed

--- sales/sales_rest/api_views.py
+++ sales/sales_rest/api_views.py
@@ -4,8 +4,10 @@ from common.json import ModelEncoder
 from django.views.decorators.http import require_http_methods
 from sales_rest.models import WineVO, Order, ShoppingItem
 import json
-
-
+import random
+# class WineryVOEncoder(ModelEncoder):
+#     model = WineryVO
+#     properties = ["id", "name", "import_href"]
 class WineVOEncoder(ModelEncoder):
     model = WineVO
     properties = [
@@ -24,8 +26,6 @@ class WineVOEncoder(ModelEncoder):
         "quantity",
         "import_href",
     ]
-
-
 class OrderEncoder(ModelEncoder):
     model = Order
     properties = [
@@ -45,8 +45,8 @@ class OrderEncoder(ModelEncoder):
         "exp_date",
         "discount_ten",
         ]
-
-
+    # def get_extra_data(self, o):
+    #     return {"user": o.user.id }
 class ShoppingItemEncoder(ModelEncoder):
     model = ShoppingItem
     properties = [
@@ -55,15 +55,12 @@ class ShoppingItemEncoder(ModelEncoder):
         "price",
         "quantity",
     ]
-
     def get_extra_data(self, o):
         return {"order_id": o.order_id.id,
             "item": {
             "id": o.item.id,
             "winery_id": o.item.winery_id
         }}
-
-
 # Show list of wines from a specific winery
 # note: because there is no WineryVO, API endpoint
 # does not know if winery doesn't exist or if
@@ -104,8 +101,6 @@ def api_show_wine(request, pk1, pk2):
             {"message": "ERROR"},
             status=400,
         )
-
-
 @require_http_methods(["GET", "POST"])
 def api_list_orders(request):
     if request.method == "GET":
@@ -125,8 +120,7 @@ def api_list_orders(request):
         #         {"message": "Order unsuccessful"},
         #         status=400, 
         #     )
-
-
+    
 # Show detail of specific order
 @require_http_methods(["GET"])
 def api_show_order(request, pk):
@@ -148,8 +142,6 @@ def api_show_order(request, pk):
             {"message": "ERROR"},
             status=400,
         )
-
-
 # Show list of shopping items from orders of specific winery
 @require_http_methods(["GET", "POST"])
 def api_list_shopping_items(request, pk1):
@@ -177,8 +169,52 @@ def api_list_shopping_items(request, pk1):
             encoder=ShoppingItemEncoder,
             safe=False,
         )  
-
-
+        # content = json.loads(request.body)
+        # shopping_cart_items = content["shopping_items"]
+        # print("*******CONTENT:", content)
+        # print("*******SHOPPING_CART_ITEMS:", shopping_cart_items)
+        # for index in range(len(shopping_cart_items)):
+        #     order_id = shopping_cart_items["order_id"]
+        #     # order_id = shopping_cart_items[int(index)]["order_id"]
+        #     print("*******ORDER_ID:", order_id)
+        #     order = Order.objects.get(id=order_id)
+        #     print("*******ORDER:", order)
+        #     shopping_cart_items[index]["order_id"] = order
+        #     winery = shopping_cart_items[int(index)]["item"]["winery_id"]
+        #     print("*******WINERY_ID:", winery)
+        #     wine = shopping_cart_items[int(index)]["item"]["id"]
+        #     print("*******WINERY:", wine)
+        #     wine_id = WineVO.objects.filter(winery_id=winery).get(id=wine)
+        #     shopping_cart_items[int(index)]["item"] = wine_id
+        #     shopping_items = ShoppingItem.objects.create(**shopping_cart_items[int(index)])
+        # return JsonResponse(
+        #     shopping_items,
+        #     encoder=ShoppingItemEncoder,
+        #     safe=False,
+        # )  
+# POST FORMAT:
+#     {
+#     "shopping_items": [
+#         {
+#             "order_id": 2,
+#             "item": {
+#                 "id": 1,
+#                 "winery_id": 1
+#             },
+#             "quantity": 2,
+#             "price": 3
+#         },
+#         {
+#             "order_id": 2,
+#             "item": {
+#                 "id": 2,
+#                 "winery_id": 1
+#             }
+#             "quantity": 2,
+#             "price": 3
+#         }
+#     ]
+#     }
 # Show list of shopping items from specific order 
 @require_http_methods(["GET", "POST"])
 def api_list_shopping_items_order(request, pk1, pk2):
--- sales/sales_rest/views.py
+++ sales/sales_rest/views.py
@@ -1,40 +1,40 @@
-from common.json import ModelEncoder
-from sales_rest.models import WineVO, Order, ShoppingItem
+# from common.json import ModelEncoder
+# from sales_rest.models import WineVO, Order, ShoppingItem
 
 
-# class WineryVOEncoder(ModelEncoder):
-#     model = WineryVO
-#     properties = ["id", "name", "import_href"]
+# # class WineryVOEncoder(ModelEncoder):
+# #     model = WineryVO
+# #     properties = ["id", "name", "import_href"]
 
 
-class WineVOEncoder(ModelEncoder):
-    model = WineVO
-    properties = [
-        "id",
-        "brand",
-        "year",
-        "varietal" "description",
-        "region",
-        "abv",
-        "volume",
-        "city_state",
-        "price",
-        "picture_url",
-        "quantity",
-        "import_href",
-    ]
+# class WineVOEncoder(ModelEncoder):
+#     model = WineVO
+#     properties = [
+#         "id",
+#         "brand",
+#         "year",
+#         "varietal" "description",
+#         "region",
+#         "abv",
+#         "volume",
+#         "city_state",
+#         "price",
+#         "picture_url",
+#         "quantity",
+#         "import_href",
+#     ]
 
 
-class OrderEncoder(ModelEncoder):
-    model = Order
-    properties = ["confirmation_number", "created"]
+# class OrderEncoder(ModelEncoder):
+#     model = Order
+#     properties = ["confirmation_number", "created"]
 
 
-class ShoppingItemEncoder(ModelEncoder):
-    model = ShoppingItem
-    properties = [
-        "order_id",
-        "item",
-        "quantity",
-        "price",
-    ]
+# class ShoppingItemEncoder(ModelEncoder):
+#     model = ShoppingItem
+#     properties = [
+#         "order_id",
+#         "item",
+#         "quantity",
+#         "price",
+#     ]
