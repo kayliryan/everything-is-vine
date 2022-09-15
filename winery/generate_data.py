de8882ab95a7551506a6ceb916a4f4e09e0d248c
Richard Scott <rich5d@icloud.com>
2022-09-15 11:10:46 -0700

    made migrations

--- winery/generate_data.py
+++ winery/generate_data.py
@@ -1,9 +1,9 @@
-# from inventory.models import  Winery
+from inventory.models import  Winery
 
-# winery1 = Winery.objects.create(
-#     name="Scott Family Vineyards",
-#     url="https://images.pexels.com/photos/442116/pexels-photo-442116.jpeg",
-#     address="2000 Main St, St. Helena CA 94574",
-#     decription="St Helena's oldest continuously operating winery. Come enjoy our wide diversity of award-winning wines, rich history & beautifully landscaped grounds.",
-#     owner="Rich Scott")
+winery1 = Winery.objects.create(
+    name="Scott Family Vineyards",
+    url="https://images.pexels.com/photos/442116/pexels-photo-442116.jpeg",
+    address="2000 Main St, St. Helena CA 94574",
+    description="St Helena's oldest continuously operating winery. Come enjoy our wide diversity of award-winning wines, rich history & beautifully landscaped grounds.",
+    owner="Rich Scott")
     
